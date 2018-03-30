5330b642b1294c99d10071b2e571115bbc1ee36d
essheva <essheva@gmail.com>
2018-03-30 18:48:00 -0400

    project structure generated by Spring Boot

---
+++ src/main/java/com/sheva/springSandbox/SpringSandboxApplication.java
@@ -0,0 +1,12 @@
+package com.sheva.springSandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSandboxApplication.class, args);
+	}
+}
