6e55a355dab9a6361a1a091ff576b987155de8bb
essheva <essheva@gmail.com>
2018-03-31 19:49:26 -0400

    - added View + Controller part for Author, Book, Publisher lists

---
+++ src/main/java/com/sheva/springSandbox/controllers/PublisherController.java
@@ -0,0 +1,22 @@
+package com.sheva.springSandbox.controllers;
+
+import com.sheva.springSandbox.repositories.PublisherRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublisherController {
+
+    private PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @RequestMapping("/publishers")
+    public String getPublishers(Model model){
+        model.addAttribute("publishers", publisherRepository.findAll());
+        return "publishers";
+    }
+}
