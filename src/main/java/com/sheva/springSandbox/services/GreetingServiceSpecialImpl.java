385cd7f772f79aa652f2a60d3e2e2dbd30295641
essheva <essheva@gmail.com>
2018-04-01 14:17:43 -0400

    - added profiles

--- src/main/java/com/sheva/springSandbox/services/GreetingServiceSpecialImpl.java
+++ src/main/java/com/sheva/springSandbox/services/GreetingServiceSpecialImpl.java
@@ -1,10 +1,12 @@
 package com.sheva.springSandbox.services;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Qualifier("specialGreeting")
+@Profile({"en", "default"})
 public class GreetingServiceSpecialImpl implements GreetingService {
 
     public static final String GREETING_MSG = "Hello, my darling!";
